2c3cfdddcfdf9ecbb0bf05ab583cf995e4c5deb3
vinaybujja <122912206+vinaybujja@users.noreply.github.com>
2026-02-05 00:53:57 +0530

    Add files via upload

---
+++ Velantaine/script.js
@@ -0,0 +1,6 @@
+function she_said_yes() {
+  document.getElementById("response").style.display = "flex";
+  window.scrollTo({ top: document.body.scrollHeight, // scroll to the bottom 
+  behavior: "smooth" // smooth animation 
+  });
+}
